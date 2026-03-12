cbfaeb943de2929f734d8c7199da68ae36466512
Jasher <jasherlim32@gmail.com>
2026-03-12 18:42:53 +0800

    Add MembershipId import to AddCommandTest

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -24,6 +24,7 @@ import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
+import seedu.address.model.person.MembershipId;
 
 public class AddCommandTest {
 
