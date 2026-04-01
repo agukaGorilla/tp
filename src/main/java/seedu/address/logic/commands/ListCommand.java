9b79aed14c8af5b9dbb6162fe446f5bb86a9c38d
SUN_HEQIAO <e1355839@u.nus.edu>
2026-04-01 19:45:31 +0800

    Message when there is no one to list

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -13,12 +13,18 @@ public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_NO_PERSONS = "No person to list";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        if (model.getFilteredPersonList().isEmpty()) {
+            return new CommandResult(MESSAGE_NO_PERSONS);
+        }
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
